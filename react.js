32d7dc0370a33d603072ae74bf33d79923135db5
Sam Garson <samtgarson@gmail.com>
2022-07-01 22:46:03 +0100

    Extend from react/jsx-runtime for react apps

--- react.js
+++ react.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime'
   ],
   rules: {
     'react/prop-types': 0
