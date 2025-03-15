929067c745dc47ed74cf9e0a7d43710814d32fe5
Sam Garson <samtgarson@gmail.com>
2025-03-15 14:37:10 +0000

    Convert to flat config properly

--- typescript.js
+++ typescript.js
@@ -1,30 +1,34 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript'
-  ],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/member-delimiter-style': [
-      2,
-      {
+import { defineConfig } from "eslint/config"
+import eslintPluginImportX from 'eslint-plugin-import-x'
+import tseslint from 'typescript-eslint'
+
+
+export default defineConfig([
+  tseslint.configs.recommendedTypeChecked,
+  eslintPluginImportX.flatConfigs.typescript,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": 0,
+      "@typescript-eslint/member-delimiter-style": [2, {
         multiline: {
-          delimiter: 'none'
+          delimiter: "none",
         },
+
         singleline: {
-          delimiter: 'semi'
-        }
-      }
-    ],
-    '@typescript-eslint/no-use-before-define': 0,
-    '@typescript-eslint/no-empty-function': 0,
-    '@typescript-eslint/ban-ts-ignore': 0,
-    '@typescript-eslint/no-unused-vars': 1,
-    "@typescript-eslint/explicit-module-boundary-types": 0
-  }
-}
+          delimiter: "semi",
+        },
+      }],
+      "@typescript-eslint/no-use-before-define": 0,
+      "@typescript-eslint/no-empty-function": 0,
+      "@typescript-eslint/ban-ts-ignore": 0,
+      "@typescript-eslint/no-unused-vars": 1,
+      "@typescript-eslint/explicit-module-boundary-types": 0,
+    },
+  }])
